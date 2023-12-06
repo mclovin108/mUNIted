cbd91e6e0718d581380e2a5558165d59f28deff1
dts <daniel.seitz@hm.edu>
2023-12-06 15:12:23 +0100

    fix: Fix create-Route

---
+++ frontend/lib/eventdetails.dart
@@ -0,0 +1,23 @@
+class EventDetails {
+  final String title;
+  final String creator;
+  final String category;
+  final int participants;
+  final int maxParticipants;
+  final int dayDate;
+  final String monthDate;
+  final String location;
+  final String description;
+
+  EventDetails({
+    required this.title,
+    required this.creator,
+    required this.category,
+    required this.participants,
+    required this.maxParticipants,
+    required this.dayDate,
+    required this.monthDate,
+    required this.location,
+    required this.description,
+  });
+}
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:munited/Backend/backend.dart';
-import 'package:munited/Screens/Create/createpage.dart';
+import 'package:munited/Screens/CreateMeeting/create_meeting.dart';
 import 'package:munited/Screens/Dashboard/dashboard.dart';
 import 'package:munited/Screens/Detail/detailpage.dart';
 import 'package:munited/Screens/Login/login_screen.dart';
 import 'Screens/Signup/signup_screen.dart';
 import 'package:provider/provider.dart';
-import 'package:munited/model/UserProvider.dart';
+import 'package:munited/model/user_provider.dart';
 
 
 void main() {
@@ -30,10 +30,10 @@ class MyApp extends StatelessWidget {
         initialRoute: '/signup', // Set the initial route
         routes: {
           '/signup': (context) => SignupPage(Backend(), http.Client()),
-          '/dash': (context) => Dashboard(),
+          '/dash': (context) => Dashboard(Backend(), http.Client()),
           '/login': (context) => LoginPage(Backend(), http.Client()),
           '/detail': (context) => Detail(Backend(), http.Client()),
-          '/create': (context) => CreatePage(),
+          '/create': (context) => CreateMeetingPage(Backend(), http.Client()),
         },
       ),
     );
