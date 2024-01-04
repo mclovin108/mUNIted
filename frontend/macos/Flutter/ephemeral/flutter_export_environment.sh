bac6a33743cce2103ede61b074a8a34fd48dbc23
hm-wessel0 <lucas.wessel@hm.edu>
2024-01-04 19:59:26 +0100

    fix: Frontend tests

--- frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ frontend/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\dts\Documents\CodingProjects\FlutterProjects\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\dts\Documents\CodingProjects\FlutterProjects\project_24\frontend"
+export "FLUTTER_ROOT=/Users/lucaswessel/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/lucaswessel/Desktop/Studium/Semester III/Software Engineering/project_24-5/frontend"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/lucaswessel/Desktop/Studium/Semester III/Software Engineering/project_24-5/frontend/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9hNzk0Y2YyNjgxYzZjOWZlN2IyNjBlMGU4NGRlOTYyOThkYzljMThiLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/lucaswessel/Desktop/Studium/Semester III/Software Engineering/project_24-5/frontend/.dart_tool/package_config.json"
